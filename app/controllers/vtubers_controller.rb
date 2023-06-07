ffe18a4c770e3dc34ba1c60011ac4854e8ca69dc
KaguyaYou <127382105+KaguyaYou@users.noreply.github.com>
2023-06-07 13:11:06 +0000

    update vtuber view

--- app/controllers/vtubers_controller.rb
+++ app/controllers/vtubers_controller.rb
@@ -2,18 +2,27 @@ class VtubersController < ApplicationController
   def index
     @total_vtubers =Vtuber.all
     @vtubers = Vtuber.all.page(params[:page]).per(8)
+    @vutuber= Vtuber.new
   end
 
   def show
-
+    @vtuber = Vtuber.find(params[:id])
+    @vtuber_new = Vtuber.new
+    @vtuber_comment = VtuberComment.new
   end
 
   def edit
-
+    @vtuber = Vtuber.find(params[:id])
   end
 
   def create
-
+    @vtuber = Vtuber.new(vtuber_params)
+    @vtuber.user_id = current_user_id
+    if @vtuber.save
+      flash[:notice]
+      redirect_to vtuber_path(@vtuber)
+    else
+    end
   end
 
   def destroy
--- db/migrate/20230605023300_create_vtubers.rb
+++ db/migrate/20230605023300_create_vtubers.rb
@@ -1,9 +1,9 @@
 class CreateVtubers < ActiveRecord::Migration[6.1]
   def change
     create_table :vtubers do |t|
-      t.string :vtuber_id
       t.string :name
       t.text :body
+      t.integer :user_id
       t.timestamps
     end
   end

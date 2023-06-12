7109c39a979568ecb1f28f2c1a4957955ac3eb95
KaguyaYou <127382105+KaguyaYou@users.noreply.github.com>
2023-06-12 12:15:13 +0000

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# puts "------------vtuber create-----------"
 
-# vtuber1 = Vtuber.create!(
-#   name: "霜月 美雪（しもつき みゆき）"
-#   belonging_office: "koon"
-  
+# puts "----------"
 
-  
-  # )
+puts "------------vtuber create-----------"
+
+  vtuber = Vtuber.create!(
+
+  name: "霜月 美雪（しもつき みゆき）",
+  belonging_office: "雪月花",
+  user_id: "1",
+  fan_name: "みゆき",
+  debut_day: '2023/06/08',
+  registered_person: '10000',
+  profile: "こんにちは、よろしくお願い致します。",
+  category: 'koon'
+)
+  vtuber.image.attach(io: File.open('./app/assets/images/Shimotsuki.jpg'), filename: 'Shimotsuki.jpg')
+  vtuber.save!

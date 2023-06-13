cf0c249ca852090fd0686880572efab75d07bbc1
KaguyaYou <127382105+KaguyaYou@users.noreply.github.com>
2023-06-13 11:42:04 +0000

    update

--- db/migrate/20230613052249_create_chat_rooms.rb
+++ db/migrate/20230613052249_create_chat_rooms.rb
@@ -2,7 +2,8 @@ class CreateChatRooms < ActiveRecord::Migration[6.1]
   def change
     create_table :chat_rooms do |t|
       t.string :name
-
+      t.integer :user_id
+      t.integer :room_id
       t.timestamps
     end
   end

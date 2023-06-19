66a75c0eba9ae81154bb4e333e7db2f45c79c971
KaguyaYou <127382105+KaguyaYou@users.noreply.github.com>
2023-06-19 06:50:52 +0000

    update migrate

--- db/schema.rb
+++ db/schema.rb
@@ -75,16 +75,6 @@ ActiveRecord::Schema.define(version: 2023_06_17_062601) do
     t.index ["name"], name: "index_groups_on_name", unique: true
   end
 
-  create_table "post_tags", force: :cascade do |t|
-    t.integer "post_id", null: false
-    t.integer "tag_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["post_id", "tag_id"], name: "index_post_tags_on_post_id_and_tag_id", unique: true
-    t.index ["post_id"], name: "index_post_tags_on_post_id"
-    t.index ["tag_id"], name: "index_post_tags_on_tag_id"
-  end
-
   create_table "tags", force: :cascade do |t|
     t.string "tag_name", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -141,8 +131,6 @@ ActiveRecord::Schema.define(version: 2023_06_17_062601) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "group_users", "groups"
   add_foreign_key "group_users", "users"
-  add_foreign_key "post_tags", "posts"
-  add_foreign_key "post_tags", "tags"
   add_foreign_key "vtuber_tags", "tags"
   add_foreign_key "vtuber_tags", "vtubers"
 end

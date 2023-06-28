8147a2ea346fa0c5b18d1cf1ecc91e2721dc91d3
KaguyaYou <127382105+KaguyaYou@users.noreply.github.com>
2023-06-28 01:49:10 +0000

    update bug

--- app/controllers/vtubers_controller.rb
+++ app/controllers/vtubers_controller.rb
@@ -1,5 +1,5 @@
 class VtubersController < ApplicationController
-  before_action :authenticate_user!, only:[:new, :create]
+  before_action :authenticate_user!
   before_action :is_matching_login_user, only: [:edit, :update]
 
   def index
